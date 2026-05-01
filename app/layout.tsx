af4843239c6d03f63471d696da33c5ffce21bcd9
Leejun <estherleejunkim@gmail.com>
2026-05-01 15:03:03 +0900

    make bg darker

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
 
       <GoogleAnalytics gaId="G-VEJVKJLKK7" />
 
-      <body className="flex h-screen flex-col overflow-hidden bg-[#e4ecff] dark:bg-[#26263d] dark:text-[#fffdf7]">
+      <body className="flex h-screen flex-col overflow-hidden bg-[#e4ecff] dark:bg-[#232338] dark:text-[#fffdf7]">
         <div className="pt-0.5 pb-0.5">
           <Header />
         </div>
